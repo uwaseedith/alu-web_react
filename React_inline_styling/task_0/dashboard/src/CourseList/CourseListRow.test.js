173dce2fae822089e99a738ec8b52727d0275c49
uwaseedith <109415728+uwaseedith@users.noreply.github.com>
2024-09-20 14:24:07 +0200

    Update CourseListRow.test.js

--- React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.test.js
+++ React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.test.js
@@ -2,26 +2,22 @@ import React from "react";
 import CourseListRow from "./CourseListRow";
 import { render, screen } from "@testing-library/react";
 
-describe("CourseListRow component", () => {
-  it("should render both headers when isHeader is true and both cells are provided", () => {
-    render(<CourseListRow isHeader={true} textFirstCell="First" textSecondCell="Second" />);
-    
-    expect(screen.getByText("First")).toBeInTheDocument();
-    expect(screen.getByText("Second")).toBeInTheDocument();
-  });
+describe("Course List row", () => {
+    it("should render the first and second if the isHeader is true", () => {
+        render(<CourseListRow isHeader={true} textFirstCell="First" textSecondCell="Second" />);
+        screen.getByText("First");
+        screen.getByText("Second");
+    });
 
-  it("should render only the first cell with colspan=2 when isHeader is true and the second cell is null", () => {
-    render(<CourseListRow isHeader={true} textFirstCell="First" textSecondCell={null} />);
-    
-    const firstCell = screen.getByText("First");
-    expect(firstCell).toBeInTheDocument();
-    expect(firstCell).toHaveAttribute("colSpan", "2");
-  });
+    it("should render only the first element if the isHeader is true and the second element is null", () => {
+        render(<CourseListRow isHeader={true} textFirstCell="First" textSecondCell={null} />);
+        screen.getByText("First");
+    });
 
-  it("should render both cells when isHeader is false", () => {
-    render(<CourseListRow isHeader={false} textFirstCell="First" textSecondCell="Second" />);
-    
-    expect(screen.getByText("First")).toBeInTheDocument();
-    expect(screen.getByText("Second")).toBeInTheDocument();
-  });
-});
+    it("should render the two element if the header is false", () => {
+        render(<CourseListRow isHeader={false} textFirstCell="First" textSecondCell="Second" />);
+        screen.getByText("First");
+        screen.getByText("Second"); 
+    })
+
+})
